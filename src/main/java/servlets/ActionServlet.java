888b6b69c58dea036cbb1416aeb230fe45e9dab7
Iuliia Savich <Iuliia_Savich@epam.com>
2016-06-15 16:13:13 +0300

    add PRG pattern

--- src/main/java/servlets/ActionServlet.java
+++ src/main/java/servlets/ActionServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -40,6 +41,8 @@ public class ActionServlet extends HttpServlet {
         HttpSession session = req.getSession();
         session.setAttribute("name", name);
         session.setAttribute("password", password);
+
+        resp.sendRedirect("/ActionServlet");
 
         out.close();
     }
